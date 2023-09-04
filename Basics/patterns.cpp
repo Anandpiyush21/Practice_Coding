824fae2f4bcb7f624e1b83dd3d47c0a6a4d34b58
Piyush <anandpiyush00@gmail.com>
2023-09-04 10:59:23 +0530

    vs_code Repair

---
+++ Basics/patterns.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long
+using namespace std;
+
+
+signed main(void){
+fastIO;
+
+
+
+
+return 0;
+}
