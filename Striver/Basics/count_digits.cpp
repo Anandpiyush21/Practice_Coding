250dea86dd1cccb84598d600f06e8cdb00230785
Piyush <anandpiyush00@gmail.com>
2023-09-08 16:27:01 +0530

    Count_digits

--- Striver/Basics/count_digits.cpp
+++ Striver/Basics/count_digits.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 
 int count_digits(int n){
+    int c=0;
     while(n>0){
-        
+        n=n/10;
+        c++;
     }
+    return c;
 }
 
 int main(){
-
-
+cout<<count_digits(3)<<endl;
+cout<<count_digits(33)<<endl;
+cout<<count_digits(433)<<endl;
+cout<<count_digits(3345)<<endl;
 return 0;
 }
