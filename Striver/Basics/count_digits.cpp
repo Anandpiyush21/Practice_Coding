085954fe1466263dba019c76f94ce4cf63bc7038
Piyush <anandpiyush00@gmail.com>
2023-09-08 16:38:09 +0530

    Count Digits 2

--- Striver/Basics/count_digits.cpp
+++ Striver/Basics/count_digits.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-//#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int count_digits(int n){
+int count_digits1(int n){
     int c=0;
     while(n>0){
         n=n/10;
@@ -11,10 +11,15 @@ int count_digits(int n){
     return c;
 }
 
+int count_digits2(int n){
+    int cnt = (int)(log10(n)+1);
+    return cnt;
+}
+
 int main(){
-cout<<count_digits(3)<<endl;
-cout<<count_digits(33)<<endl;
-cout<<count_digits(433)<<endl;
-cout<<count_digits(3345)<<endl;
+cout<<count_digits1(3)<<endl;
+cout<<count_digits2(33)<<endl;
+cout<<count_digits2(433)<<endl;
+cout<<count_digits1(3345)<<endl;
 return 0;
 }
