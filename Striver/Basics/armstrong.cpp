dc35c3a38dcf0161cece13e6ebe6ebf939e00833
Piyush <anandpiyush00@gmail.com>
2023-09-08 19:27:21 +0530

    Armstrong update

--- Striver/Basics/armstrong.cpp
+++ Striver/Basics/armstrong.cpp
@@ -2,13 +2,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int count_digits1(int n){
+    int c=0;
+    while(n>0){
+        n=n/10;
+        c++;
+    }
+    return c;
+}
+
 int sum_arm(int n){
     int sum=0;
+    int k=n;
+    int ld;
     while(n>0){
-        int ld=n%10;
-        sum=sum+pow(ld,4);
+        ld=n%10;
+        sum=sum+pow(ld,count_digits1(k));
+        // cout<<sum<<" ";
         n/=10;
     }
+    // cout<<"\n";
     return sum;
 }
 
@@ -17,9 +30,10 @@ bool isArm(int n){
 }
 
 int main(){
-
+// cout<<count_digits1(1634)<<" "<<count_digits1(371)<<endl;
 cout<<isArm(1634)<<endl;
-cout<<isArm(634)<<endl;
+cout<<isArm(371)<<endl;
+
 
 
 return 0;
