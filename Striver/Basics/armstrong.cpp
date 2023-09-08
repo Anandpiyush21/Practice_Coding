286333c4d2fa76f54ae00025b0761fa958dfb5f2
Piyush <anandpiyush00@gmail.com>
2023-09-08 16:53:48 +0530

    Armstrong

---
+++ Striver/Basics/armstrong.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum_arm(int n){
+    int sum=0;
+    while(n>0){
+        int ld=n%10;
+        sum=sum+pow(ld,4);
+        n/=10;
+    }
+    return sum;
+}
+
+bool isArm(int n){
+    return (n=sum_arm(n));
+}
+
+int main(){
+
+cout<<isArm(1634);
+
+return 0;
+}
