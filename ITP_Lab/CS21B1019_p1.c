6c74a795734e51c8cfedf11c646a1b47df9c1dd2
Piyush <anandpiyush00@gmail.com>
2023-09-24 15:13:17 +0530

    Lab_Ques

--- ITP_Lab/CS21B1019_p1.c
+++ ITP_Lab/CS21B1019_p1.c
@@ -1,44 +1,72 @@
 // Method 1 using While loop
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int n;
-    printf("Enter a natural number (n): ");
-    scanf("%d", &n);
+// int main() {
+//     int n;
+//     scanf("%d", &n);
 
-    int i = 1;
-    while (i <= n) {
-        if (i % 5 == 0 && i % 7 == 0) {
-            printf("cheemdoge ");
-        } else if (i % 5 == 0) {
-            printf("cheem ");
-        } else if (i % 7 == 0) {
-            printf("doge ");
-        } else {
-            printf("%d ", i);
-        }
-        i++;
-    }
+//     int i = 1;
+//     while (i <= n) {
+//         if (i % 5 == 0 && i % 7 == 0) {
+//             printf("cheemdoge \n");
+//         } else if (i % 5 == 0) {
+//             printf("cheem \n");
+//         } else if (i % 7 == 0) {
+//             printf("doge \n");
+//         } else {
+//             printf("%d \n", i);
+//         }
+//         i++;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 // Method 2 using For loop
+// #include <stdio.h>
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++) {
+//         if (i % 5 == 0 && i % 7 == 0) {
+//             printf("cheemdoge \n");
+//         } else if (i % 5 == 0) {
+//             printf("cheem \n");
+//         } else if (i % 7 == 0) {
+//             printf("doge \n");
+//         } else {
+//             printf("%d \n", i);
+//         }
+//     }
+
+//     return 0;
+// }
+
+// Using switch
 #include <stdio.h>
 
 int main() {
     int n;
-    printf("Enter a natural number (n): ");
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++) {
-        if (i % 5 == 0 && i % 7 == 0) {
-            printf("cheem doge ");
-        } else if (i % 5 == 0) {
-            printf("cheem ");
-        } else if (i % 7 == 0) {
-            printf("doge ");
-        } else {
-            printf("%d ", i);
+        int db5 = (i % 5 == 0);
+        int divisibleBy7 = (i % 7 == 0);
+
+        switch (db5 * 2 + divisibleBy7) {
+            case 0:
+                printf("%d\n", i);
+                break;
+            case 1:
+                printf("doge\n");
+                break;
+            case 2:
+                printf("cheem\n");
+                break;
+            case 3:
+                printf("cheemdoge\n");
+                break;
         }
     }
 
