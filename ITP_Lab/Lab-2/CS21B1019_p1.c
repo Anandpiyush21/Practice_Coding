ae5d4aef8aabc29a78fc1a2c2769dda8aef5750d
Piyush <anandpiyush00@gmail.com>
2023-09-28 15:30:25 +0530

    Q1

--- ITP_Lab/Lab-2/CS21B1019_p1.c
+++ ITP_Lab/Lab-2/CS21B1019_p1.c
@@ -4,7 +4,7 @@ int main(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=10;i++){
-        if((i*n)%10==0) printf("%d * %d = Yeahh\n",n,i);
-        else printf("%d * %d = %d\n",n,i,i*n);
+        if((i*n)%10)printf("%d * %d = %d\n",n,i,i*n);
+        else printf("%d * %d = Yeahh\n",n,i);
     }
 }
