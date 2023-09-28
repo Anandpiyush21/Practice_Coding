31722d67337a26dcb543c11f70078e5207d64950
Piyush <anandpiyush00@gmail.com>
2023-09-28 15:54:56 +0530

    Q1

--- ITP_Lab/Lab-2/CS21B1019_p1.c
+++ ITP_Lab/Lab-2/CS21B1019_p1.c
@@ -4,7 +4,6 @@ int main(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=10;i++){
-        if((i*n)%10)printf("%d * %d = %d\n",n,i,i*n);
-        else printf("%d * %d = Yeahh\n",n,i);
+        ((i*n)%10)?printf("%d * %d = %d\n",n,i,i*n):printf("%d * %d = Yeahh\n",n,i);
     }
 }
