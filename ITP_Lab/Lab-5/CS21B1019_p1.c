c54e3e4c502c30deaf486cf0d141525f147ca420
Piyush <anandpiyush00@gmail.com>
2023-11-04 15:15:42 +0530

    Done

--- ITP_Lab/Lab-5/CS21B1019_p1.c
+++ ITP_Lab/Lab-5/CS21B1019_p1.c
@@ -1,27 +1,28 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int searchInsert(int* nums, int numsSize, int target){
-    int i = 0;
+// int searchInsert(int* nums, int numsSize, int target){
+//     int i = 0;
     
-    while ((i < numsSize) && (target > nums[i])) {
-        i ++;
-    }
+//     while ((i < numsSize) && (target > nums[i])) {
+//         i ++;
+//     }
     
-    return i;
-}
+//     return i;
+// }
 
-int main() {
-    int n;
-    int k;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&k);
-        arr[i]=k;
-    }
-    int tar;
-    scanf("%d",&tar);
-    int result = searchInsert(arr, n, tar);
-    printf("%d\n", result);
-    return 0;
-}
+// int main() {
+//     int n;
+//     int k;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&k);
+//         arr[i]=k;
+//     }
+//     int tar;
+//     scanf("%d",&tar);
+//     int result = searchInsert(arr, n, tar);
+//     printf("%d\n", result);
+//     return 0;
+// }
+// Incomplete
